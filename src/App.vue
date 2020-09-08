86b54febb90c4a450cc65222de2bbe45d33d5bd0
刘宇涵 <1316637389@qq.com>
2020-09-08 18:28:03 +0800

    第三次修改

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,27 @@
  * @Author: 刘宇涵
  * @Date: 2020-09-08 16:43:14
  * @LastEditors: 刘宇涵
- * @LastEditTime: 2020-09-08 17:13:22
+ * @LastEditTime: 2020-09-08 17:29:43
 -->
 <template>
   <div id="app">
    这是我修改后得vue项目
+   <Index :a="msg"/>
   </div>
 </template>
 
 <script>
-
+  import Index from './components/Index'
 export default {
   name: 'App',
+  data(){
+    return {
+       msg:"这是我要传给子组件得字符串"
+    }
+     
+  },
   components: {
-   
+    Index
   }
 }
 </script>
---
+++ src/components/Index.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: 刘宇涵
+ * @Date: 2020-09-08 17:17:10
+ * @LastEditors: 刘宇涵
+ * @LastEditTime: 2020-09-08 17:33:53
+-->
+<template>
+  <div class="box">
+        <p>{{a}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"box",
+    props:{
+        a:String
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
